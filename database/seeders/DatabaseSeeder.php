cf293ec37d2083405296f4d0297c8b3a65d05a59
MirkoFarina <MirkoFarina@outlook.com>
2023-01-23 16:03:52 +0100

    [add] seeder for id type in projects

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             ProjectTableSeeder::class,
-            TypeSeeder::class
+            TypeSeeder::class,
+            ProjectTypeSeeder::class
         ]);
     }
 }
